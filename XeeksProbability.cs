7374ce461f6b7b1df16399fd302c446e74039abd
Xeek <xeek@xeekworx.com>
2021-01-30 10:32:00 -0600

    Moved XeeksProbabilityValue to it's own source file

--- XeeksProbability.cs
+++ XeeksProbability.cs
@@ -1,7 +1,4 @@
-﻿using Sirenix.OdinInspector;
-using Sirenix.Serialization;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -70,42 +67,6 @@ namespace Xeek
                 x.End = offset + x.Probability;
                 offset = x.End + 1;
             });
-        }
-    }
-
-    [DebuggerDisplay("Comment = {Comment}, Value = {Value}, Offset Range = {Start} - {End}")]
-    public class XeeksProbabilityValue
-    {
-        [OdinSerialize]
-        public string Comment { get; set; }
-
-        [OdinSerialize]
-        public int Value { get; set; }
-
-        [OdinSerialize]
-        [InfoBox("For a very high probability, consider using \"Repeatable\" for that probability", 
-            InfoMessageType.None, VisibleIf = nameof(IsHighProbabilityAndRepeatableNotAllowed))]
-        [PropertyRange(0, 1000)]
-        public int Probability { get; set; }
-
-        private bool IsHighProbabilityAndRepeatableNotAllowed => Probability >= 350 && !Repeatable;
-
-        [OdinSerialize]
-        public bool Repeatable { get; set; } = false;
-
-        [OdinSerialize]
-        [ReadOnly]
-        [HorizontalGroup("Range", MarginRight = 0.025f)]
-        public int Start { get; set; }
-
-        [OdinSerialize]
-        [ReadOnly]
-        [HorizontalGroup("Range")]
-        public int End { get; set; }
-
-        public XeeksProbabilityValue(int value = 0)
-        {
-            Value = value;
         }
     }
 }
---
+++ XeeksProbabilityValue.cs
@@ -0,0 +1,42 @@
+﻿using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using System.Diagnostics;
+
+namespace Xeek
+{
+    [DebuggerDisplay("Comment = {Comment}, Value = {Value}, Offset Range = {Start} - {End}")]
+    public class XeeksProbabilityValue
+    {
+        [OdinSerialize]
+        public string Comment { get; set; }
+
+        [OdinSerialize]
+        public int Value { get; set; }
+
+        [OdinSerialize]
+        [InfoBox("For a very high probability, consider using \"Repeatable\" for that probability", 
+            InfoMessageType.None, VisibleIf = nameof(IsHighProbabilityAndRepeatableNotAllowed))]
+        [PropertyRange(0, 1000)]
+        public int Probability { get; set; }
+
+        private bool IsHighProbabilityAndRepeatableNotAllowed => Probability >= 350 && !Repeatable;
+
+        [OdinSerialize]
+        public bool Repeatable { get; set; } = false;
+
+        [OdinSerialize]
+        [ReadOnly]
+        [HorizontalGroup("Range", MarginRight = 0.025f)]
+        public int Start { get; set; }
+
+        [OdinSerialize]
+        [ReadOnly]
+        [HorizontalGroup("Range")]
+        public int End { get; set; }
+
+        public XeeksProbabilityValue(int value = 0)
+        {
+            Value = value;
+        }
+    }
+}
